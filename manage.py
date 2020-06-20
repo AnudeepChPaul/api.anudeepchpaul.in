71fa18ed355556abf02dadf926a327ed712abdae
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-06-20 12:36:53 +0530

    modified procfile

--- manage.py
+++ manage.py
@@ -3,14 +3,12 @@ from src.db import init_db_connection
 import src.configurations as config
 
 
-def start():
+application = app
+
+if __name__ == '__main__':
     init_db_connection()
-    app.run(
+    application.run(
         host=config.DEV_HOST,
         port=config.DEV_PORT,
         debug=config.DEV_DEBUG
     )
-
-
-if __name__ == '__main__':
-    start()
